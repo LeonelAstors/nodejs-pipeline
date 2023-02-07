abf74b0fd6e276cdeef78ad0ec01180182474699
LeonelAstors <lakoro@astor-sanders.com>
2023-02-07 15:34:25 -0500

    changes

--- src/server.js
+++ src/server.js
@@ -3,11 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-<<<<<<< HEAD
-    response.end("Hello, Welcome to Astor-Sanders");
-=======
-    response.end("Hello, Welcome to Astor-Sanders DevOps CI CD");
->>>>>>> 0a8e28dc60cb8356086017000e517680e2ec73d9
+    response.end("Hello, Welcome to DevOps CI CD");
 
 });
 
@@ -15,3 +11,4 @@ var port = 3000;
 server.listen(port);
 module.exports = server
 console.log("Server is available on Port:", port);
+
