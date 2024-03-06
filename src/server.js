77819b13ee18517b00c18b5011a2ae09f774a13f
LeonelAstors <lakoro@astor-sanders.com>
2024-03-06 10:35:50 -0500

    chaning display message

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to Astor-Sanders DevOps CI CD");
+    response.end("Hello, Welcome to Astor-Sanders DevSecOps CI CD");
 
 });
 
@@ -11,4 +11,3 @@ var port = 3000;
 server.listen(port);
 module.exports = server
 console.log("Server is available on Port:", port);
-
